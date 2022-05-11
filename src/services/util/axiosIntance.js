334b40de62e90bd573ce931983ae75cba36b07bd
AlbertoGonzalezProg <58387793+AlbertoGonzalezProg@users.noreply.github.com>
2022-05-11 19:37:46 +0200

    Solución de error para prevenir que la cabecera sea enviada con valor no válido

--- src/services/util/axiosIntance.js
+++ src/services/util/axiosIntance.js
@@ -9,14 +9,14 @@ import { ACCESS_TOKEN, API_BASE_URL } from "../../util/constants";
 export const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
     headers: {
-        Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+        Authorization: localStorage.getItem(ACCESS_TOKEN) ? `Bearer ${localStorage.getItem(ACCESS_TOKEN)}` : "",
     }
 })
 
 axiosInstance.interceptors.request.use( function(config) {
 
     if (localStorage.getItem(ACCESS_TOKEN)) {
-        config.headers["Authorization"] = `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`;
+        config.headers["Authorization"] = localStorage.getItem(ACCESS_TOKEN) ? `Bearer ${localStorage.getItem(ACCESS_TOKEN)}` : "";
     }
  
     return config;
