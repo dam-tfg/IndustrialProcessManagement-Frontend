a5aab688f08b281d12fb28978cf232aac7b07d46
AlbertoGonzalezProg <58387793+AlbertoGonzalezProg@users.noreply.github.com>
2022-05-16 20:47:39 +0200

    Adaptación del logo

--- src/pages/auth/auth.jsx
+++ src/pages/auth/auth.jsx
@@ -3,14 +3,14 @@
  *
  */
 import logo from '../../image/logo.png';
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import styles from '../../scss/pages/auth/auth.module.scss';
 
 export const Auth = () => {
 
     return (
         <header className={styles.loginHeader}>
-            <img src={logo} className="App-logo" alt="logo" />
+            <Link to="/" id={styles.logo}/>
             <Outlet/>
         </header>
     );
